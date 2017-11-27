54b067ee9fbe7ab07902556334d0458a55737e13
Pete <peter@geektalent.com>
2017-11-27 17:53:10 +0000

    Example of parse

--- app/js/controllers/CompileSampleController.js
+++ app/js/controllers/CompileSampleController.js
@@ -1,9 +1,29 @@
 'use strict';
 
+// bad, opens up to javascript injection!
 eventsApp.controller('CompileSampleController',
-    function CompileSampleController($scope, $compile) {
+    function CompileSampleController($scope, $compile, $parse) {
         $scope.appendToDivElement = function(markup) {
             return $compile(markup) ($scope).appendTo(angular.element("#appendHere"));
         }
+
+        var fn = $parse('1 + 2');
+        console.log(fn());
+
+
+        var getter = $parse('event.name');
+
+        var context1 = {event: {name: 'AngularJS Boot Camp'}};
+        var context2 = {event: {name: 'Code Camp'}};
+
+        console.log(getter(context1));
+        console.log(getter(context2));
+
+        console.log(getter(context2, context1));
+
+        var setter = getter.assign;
+        setter(context2, 'Code Retreat');
+
+        console.log(context2.event.name);
     }
 );
